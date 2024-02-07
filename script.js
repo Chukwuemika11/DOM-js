55770f2652eefe3c278488cf5c16e6c0fc1763dd
unknown <chukwuemika11>
2024-02-07 23:28:09 +0100

    Dom  In Js

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("buton").style.backgroundColor("yellow");
+
+
+document.getElementsByClassName("name").classlist.add("mdmdn")
+document.getElementsByClassName("name").classlist.remove("mdmdn")
+document.getElementsByClassName("name").classlist.toggle("mdmdn")
+document.querySelector("kjrjr").getAttribute("href")
+document.querySelector("kj").setAttribute("href", "https:www.google.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://github.com/Chukwuemika11/DOM-js.git
